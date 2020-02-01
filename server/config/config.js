7a01e799e6f03d575701e8ba149134137a4c852a
CarlRiley99 <simpronc@ufl.edu>
2020-02-01 11:58:29 -0500

    Updated Database

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,8 @@
 
 module.exports = {
   db: {
-    uri: 'mongodb://Carl_Riley:123abc@ds221435.mlab.com:21435/carl-bootcamp-3', //place the URI of your mongo database here.
+    //uri: 'mongodb://Carl_Riley:123abc@ds221435.mlab.com:21435/carl-bootcamp-3', //place the URI of your mongo database here.
+    uri: 'mongodb://<dbuser>:<dbpassword>@ds345587.mlab.com:45587/swamphacks2020',
   },
   port: 8080
 };
